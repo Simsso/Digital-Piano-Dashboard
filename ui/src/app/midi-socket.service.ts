30b82a6992d8913570273d2ee18e331ae9dbbd00
Timo Denk <github@timodenk.com>
2018-05-23 17:02:05 +0200

    Font and padding style

--- ui/src/app/midi-socket.service.ts
+++ ui/src/app/midi-socket.service.ts
@@ -6,16 +6,17 @@ export class MidiSocketService {
 
   private socket;
   private callbacks: Function[];
-  private time: number = 0;
+  private time = 0;
 
   constructor() {
-    this.socket = io('http://192.168.0.121:3000');
+    this.socket = io('http://localhost:3000');
     this.callbacks = [];
     this.socket.on('MIDI_DATA', (data) => { this._handleEvent(data); });
   }
 
   _handleEvent({ deltaTime, message }) {
-    // process raaw data
+    console.log(`${message} (${deltaTime})`);
+    // process raw data
     const [status, data1, data2] = message;
     this.time += deltaTime;
     const data = { deltaTime, time: this.time, status, data1, data2 };
