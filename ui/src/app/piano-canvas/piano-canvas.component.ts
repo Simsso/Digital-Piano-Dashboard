23377af0a656ed66b08e85f31c43886ccb44c7a0
Timo Denk <github@timodenk.com>
2018-05-23 13:41:07 +0200

    Color palette

--- ui/src/app/piano-canvas/piano-canvas.component.ts
+++ ui/src/app/piano-canvas/piano-canvas.component.ts
@@ -18,8 +18,8 @@ export class PianoCanvasComponent implements OnInit, AfterViewInit {
   @ViewChild('pianoStateCanvas') canvasRef: ElementRef;
   ctx: CanvasRenderingContext2D;
 
-  private readonly width: number = 1760;
-  private readonly height: number = 1000;
+  public readonly width: number = 1760;
+  public readonly height: number = 1000;
   private readonly whiteKeyWidth: number = this.width / PianoCanvasComponent.whiteKeyCount;
   private readonly blackKeyWidth: number = this.whiteKeyWidth / 2;
 
@@ -36,6 +36,16 @@ export class PianoCanvasComponent implements OnInit, AfterViewInit {
   private renderWindowSpan = 5;
 
   private keyXPositions: number[] = [];
+
+  // colors
+  private colorWhiteKey = '#FAFAFA';
+  private colorBlackKey = '#212121';
+  private colorKeySeparator = '#BDBDBD';
+  private colorPianoTopRim = '#8B0000';
+  private colorWhiteKeyPushed = '#FF8A65';
+  private colorBlackKeyPushed = '#713131';
+  private colorHistoryBackground = '#FFFFFF';
+  private colorHistoryActive = '#BF360C';
 
   constructor(private midiInput: MidiSocketService) {
     this.midiInput.register(this.processNewMIDIData.bind(this));
@@ -64,23 +74,24 @@ export class PianoCanvasComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.ctx = this.canvasRef.nativeElement.getContext('2d');
-    this.ctx.fillStyle = '#DD0031';
     this.drawState();
   }
 
   drawPianoKeys() {
     // draw white keys first, because the black keys overlap them
     this.keysBW.forEach(this.drawKey.bind(this));
+    this.ctx.fillStyle = this.colorPianoTopRim;
+    this.ctx.fillRect(0, this.keyHistoryHeight, this.width, 5);
   }
 
   drawKey(key: PianoKey, nthKeyOfColor: number) {
     const xPosition: number = this.keyXPositions[key.getId()];
     const width: number = key.black ? this.blackKeyWidth : this.whiteKeyWidth;
     const height: number = key.black ? this.pianoHeight / 3 * 2 : this.pianoHeight;
-    let color: string = key.black ? '#1B1B1B' : '#F3F3F3';
+    let color: string = key.black ? this.colorBlackKey : this.colorWhiteKey;
 
     if (key.isPushed()) {
-      color = '#CC0031';
+      color = key.black ? this.colorBlackKeyPushed : this.colorWhiteKeyPushed;
     }
 
     this.ctx.fillStyle = color;
@@ -88,7 +99,7 @@ export class PianoCanvasComponent implements OnInit, AfterViewInit {
 
     // draw key separators
     if (!key.black && nthKeyOfColor > 0 && !key.isPushed()) {
-      this.ctx.strokeStyle = '#AAA';
+      this.ctx.strokeStyle = this.colorKeySeparator;
       this.ctx.beginPath();
       this.ctx.moveTo(xPosition, this.keyHistoryHeight);
       this.ctx.lineTo(xPosition, this.height);
@@ -110,12 +121,12 @@ export class PianoCanvasComponent implements OnInit, AfterViewInit {
   }
 
   clearKeyBar(key: PianoKey) {
-    this.ctx.fillStyle = '#FFFFFF';
+    this.ctx.fillStyle = this.colorHistoryBackground;
     this.ctx.fillRect(this.keyXPositions[key.getId()], 0, key.black ? this.blackKeyWidth : this.whiteKeyWidth, this.keyHistoryHeight);
   }
 
   drawKeyPressedBar(key: PianoKey, normalizedEvent: KeyPushEvent) {
-    this.ctx.fillStyle = '#CC0031';
+    this.ctx.fillStyle = this.colorHistoryActive;
     const x = this.keyXPositions[key.getId()];
     const y = Math.min(Math.round(normalizedEvent.getStart() * this.keyHistoryHeight), this.keyHistoryHeight);
     const width = key.black ? this.blackKeyWidth : this.whiteKeyWidth;
@@ -135,6 +146,10 @@ export class PianoCanvasComponent implements OnInit, AfterViewInit {
     if (data.status === 128) { // released
       this.keys[keyIndex].release(data.time);
     }
+  }
+
+  public setRenderWindowSpan(event) {
+    this.renderWindowSpan = Math.max(event.target.value, 1);
   }
 
 }
